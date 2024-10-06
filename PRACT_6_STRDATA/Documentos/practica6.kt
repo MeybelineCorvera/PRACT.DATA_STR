28c5e4f6aef84dcef599f01f91730ab47ec5e1e0
AbigailCorvera <160429142+MeybelineCorvera@users.noreply.github.com>
2024-10-05 19:24:44 -0600

    Add files via upload

---
+++ PRACT_6_STRDATA/Documentos/practica6.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun leerArchivo(ruta: String): String{
+    val archivo = File(ruta)
+    return archivo.readText()
+}
+
+fun escribirEnArchivos(ruta: String, content: String){
+    val archivo = File(ruta)
+    archivo.appendText(content)
+}
+
+fun main(args: Array<String>) {
+    val ruta = "C:/Users/MINEDUCYT/Documents/PRACT_6_STRDATA/Documentos/archivo.txt"
+    val contenido = leerArchivo(ruta)
+
+    /* Ejemplo 1 - Lectura de Archivos */
+    println(contenido)
+    println("\n")
+
+    /* Ejemplo 2 - Escribir un archivo */
+    val escritura = "\nEsta es una prueba de appendText."
+    escribirEnArchivos(ruta, escritura)
+    println("Creado Exitosamente")
+}
